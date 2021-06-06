99560e6fcc3a5ea131a866406832f4b3c674dbfc
blady lagos <78231413+bladylagos@users.noreply.github.com>
2021-06-06 14:23:24 -0600

    Agregre un for

--- ProbandoGitHub/src/probandogithub/ProbandoGitHub.java
+++ ProbandoGitHub/src/probandogithub/ProbandoGitHub.java
@@ -16,6 +16,9 @@ public class ProbandoGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            System.out.println("*");
+        }
     }
     
 }
