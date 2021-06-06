e0fcef7999f01f8a22263df6b55e292af8f5d706
blady lagos <78231413+bladylagos@users.noreply.github.com>
2021-06-06 14:28:29 -0600

    Agregre un if

--- ProbandoGitHub/src/probandogithub/ProbandoGitHub.java
+++ ProbandoGitHub/src/probandogithub/ProbandoGitHub.java
@@ -5,20 +5,34 @@
  */
 package probandogithub;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gerardo Lagos
  */
 public class ProbandoGitHub {
 
+    static Scanner entrada = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         for (int i = 0; i < 10; i++) {
             System.out.println("*");
         }
+
+        System.out.println("Ingrese un numero:");
+        int num = entrada.nextInt();
+
+        if (num >= 10) {
+            System.out.println("Es mayor");
+        } else {
+            System.out.println("Es menor");
+        }
     }
-    
+
 }
